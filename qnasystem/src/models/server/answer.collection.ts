eabea066e40017241da791b049642b4bdc706213
thevaishndra <chandrav306m@gmail.com>
2025-04-26 21:20:22 +0530

    data modelling

---
+++ qnasystem/src/models/server/answer.collection.ts
@@ -0,0 +1,25 @@
+import { IndexType, Permission } from "node-appwrite";
+import { answerCollection, db } from "../name";
+import { databases } from "./config";
+
+export default async function createAnswerCollection() {
+    //creating collection
+    await databases.createCollection(db, answerCollection, answerCollection, [
+            Permission.read("any"),
+            Permission.read("users"),
+            Permission.create("users"),
+            Permission.update("users"),
+            Permission.delete("users"),
+        ]);
+        console.log("Answer collection is created")
+
+        //creating attributes
+        await Promise.all([
+            databases.createStringAttribute(db, answerCollection, "content", 10000, true),
+            databases.createStringAttribute(db, answerCollection, "questionId", 50, true),
+            databases.createStringAttribute(db, answerCollection, "authoId", 50, true),
+        ]);
+        console.log("Answer Attributes Created");
+
+        
+}
