1768f1d70504face86013adcad7b9bb41e6201af
thevaishndra <chandrav306m@gmail.com>
2025-04-28 01:25:10 +0530

    data modelling

---
+++ qnasystem/src/models/server/dbSetup.ts
@@ -0,0 +1,30 @@
+import { db } from "../name"
+import createAnswerCollection from "./answer.collection"
+import createQuestionCollection from "./question.collection"
+import createCommentCollection from "./comment.collection"
+import createVoteCollection from "./vote.collection"
+import { databases } from "./config"
+
+export default async function getOrCreateDB() {
+    try {
+        await databases.get(db)
+        console.log("Database connection")
+    } catch (error) {
+        try {
+            await databases.create(db, db)
+            console.log("database created")
+            //create collections
+            await Promise.all([
+                createAnswerCollection(),
+                createQuestionCollection(),
+                createCommentCollection(),
+                createVoteCollection(),
+            ])
+            console.log("Collection created")
+            console.log("Database connected")
+        } catch (error) {
+            console.log("Error creating databases or collection", error)
+        }
+    }
+    return databases
+}
